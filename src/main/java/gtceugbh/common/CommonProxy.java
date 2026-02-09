75968234807012f29eea78c891ff710757f9bdcb
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-02-09 11:25:14 +0000

    Remove Event Priority

--- src/main/java/gtceugbh/common/CommonProxy.java
+++ src/main/java/gtceugbh/common/CommonProxy.java
@@ -4,7 +4,6 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import gregtech.api.GregTechAPI;
@@ -16,7 +15,7 @@ import gtceugbh.loaders.recipe.handlers.RecipeHandler;
 @Mod.EventBusSubscriber(modid = Tags.MODID)
 public class CommonProxy {
 
-    @SubscribeEvent(priority = EventPriority.LOW)
+    @SubscribeEvent
     public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {
         GBHLogger.log.info("Modifying Blast Recipe Handler...");
         MinecraftForge.EVENT_BUS.post(new GregTechAPI.RegisterEvent<>(null, ItemMaterialInfo.class));
