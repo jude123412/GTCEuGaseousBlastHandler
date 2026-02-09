e6612de0b7a079d4dd402c0cb8dd89aad33f1e2b
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-02-09 20:18:23 +0000

    Thanks intelij for the false bug i guess?

--- src/main/java/gtceugbh/common/CommonProxy.java
+++ src/main/java/gtceugbh/common/CommonProxy.java
@@ -3,7 +3,6 @@ package gtceugbh.common;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import gtceugbh.Tags;
@@ -13,7 +12,7 @@ import gtceugbh.loaders.recipe.handlers.RecipeHandler;
 @Mod.EventBusSubscriber(modid = Tags.MODID)
 public class CommonProxy {
 
-    @SubscribeEvent(priority = EventPriority.LOW)
+    @SubscribeEvent
     public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {
         GBHLogger.log.info("Modifying Blast Recipe Handler...");
 
