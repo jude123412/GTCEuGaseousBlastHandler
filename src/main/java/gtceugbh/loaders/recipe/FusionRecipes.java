5255e21a7f5c46ca8b2ab6443ffab2384e0e8bba
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-02-17 15:33:10 +0000

    Re-Balance Oganesson recipe

--- src/main/java/gtceugbh/loaders/recipe/FusionRecipes.java
+++ src/main/java/gtceugbh/loaders/recipe/FusionRecipes.java
@@ -1,5 +1,7 @@
 package gtceugbh.loaders.recipe;
 
+import static gregtech.api.GTValues.UV;
+import static gregtech.api.GTValues.VA;
 import static gregtech.api.recipes.RecipeMaps.FUSION_RECIPES;
 import static gregtech.api.unification.material.Materials.Oganesson;
 
@@ -11,11 +13,11 @@ public class FusionRecipes {
     public static void registerFusionRecipes() {
         if (Oganesson.hasFluid()) {
             FUSION_RECIPES.recipeBuilder()
-                    .fluidInputs(Materials.Vanadium.getFluid(FluidStorageKeys.LIQUID, 125))
-                    .fluidInputs(Materials.Americium.getFluid(FluidStorageKeys.LIQUID, 125))
-                    .fluidOutputs(Materials.Oganesson.getFluid(FluidStorageKeys.GAS, 250))
+                    .fluidInputs(Materials.Vanadium.getFluid(FluidStorageKeys.LIQUID, 16))
+                    .fluidInputs(Materials.Americium.getFluid(FluidStorageKeys.LIQUID, 16))
+                    .fluidOutputs(Materials.Oganesson.getFluid(FluidStorageKeys.GAS, 125))
                     .duration(200)
-                    .EUt(65565)
+                    .EUt(VA[UV])
                     .EUToStart(600_000_000)
                     .buildAndRegister();
 
